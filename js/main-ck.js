a8380e7b354311503efef4ebb21f8e2cfa303fad
Sean Mongey <sm@smongey.com>
2013-06-17 14:54:15 +0100

    initial

---
+++ js/main-ck.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$(".slides").maximage({fillElement:".wrap"});$(".wrap").css({height:$(window).height()}).css({width:$(window).width()});$(window).bind("resize",function(){$(".wrap").css({height:$(window).height()}).css({width:$(window).width()})});$.localScroll({duration:400,offset:{top:-100}})});
